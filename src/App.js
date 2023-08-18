d00141c5526fd059bd5514a108a3335d92225fa4
Success12016 <pattarachai12016@gmail.com>
2023-08-18 15:32:15 +0700

    eventdata

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import react from 'react'
-import logo from './logo.svg';
-import './App.css';
-import { Header, Content, Footer } from './func-components'
-import Calendar from './class-components'
-import React from 'react'
 import Banner from './banner'
 import {Calculator} from './calculator'
 import {Calculator2} from './calculator'
+import {EventData2} from './event-data'
 
 
 export default function App() {
 return (
     <>
-    <Header/>
     <Banner/>
-    <p><center><Calendar/></center></p>
-    <Content/>
     <Calculator/>
     <Calculator2/>
-    <Footer/>
+    <EventData2/>
     </>
     )
      
