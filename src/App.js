b37dc8b9a8aefc87a51ade64fd6c5fdbbb05319d
Success12016 <pattarachai12016@gmail.com>
2023-08-25 15:12:39 +0700

    refs-array done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import react from 'react'
 import Banner from './banner'
-import {Calculator} from './calculator'
-import {Calculator2} from './calculator'
-import {EventData2} from './event-data'
 import RefsFunc from './refs-func'
+import RefsArray from './refs-array'
 
 export default function App() {
 return (
     <>
     <Banner/>
-    <Calculator/>
-    <Calculator2/>
-    <EventData2/>
     <RefsFunc/>
+    <RefsArray/>
     </>
     )
      
