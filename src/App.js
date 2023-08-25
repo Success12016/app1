00edd7da9b5380b783c34b2ac71da22978c71450
Success12016 <pattarachai12016@gmail.com>
2023-08-25 15:41:00 +0700

    state-func done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import react from 'react'
 import Banner from './banner'
 import RefsFunc from './refs-func'
 import RefsArray from './refs-array'
+import MessageBox from './state-func'
 
 export default function App() {
 return (
@@ -9,6 +10,7 @@ return (
     <Banner/>
     <RefsFunc/>
     <RefsArray/>
+    <MessageBox/>
     </>
     )
      
